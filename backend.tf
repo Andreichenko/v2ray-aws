df770e7348d345c5d107f146f3c37b318e285a97
aleksandrandreichenko <andreichenko@web.de>
2021-05-06 15:17:16 +0300

    backend init file

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version    =    ">= 0.12.0"
+  required_providers {
+    aws = ">=3.0.0"
+  }
+  backend "s3" {
+    region            = "eu-central-1"
+    profile           = "default"
+    key               = "terraform-state-file/statefile.tfstate"
+    bucket            = "terraform-state-bucket-andreichenko-0009"
+  }
+}
