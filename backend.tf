fff1101bf959f2929500ce8935debeba42f959a7
aleksandrandreichenko <andreichenko@web.de>
2021-05-12 11:22:06 +0300

    backend init file

--- backend.tf
+++ backend.tf
@@ -8,6 +8,6 @@ terraform {
     region  = "eu-central-1"
     profile = "default"
     key     = "terraform-state-file/statefile.tfstate"
-    bucket  = "terraform-state-bucket"
+    bucket  = "terraform-state-cname-bucket"
   }
 }
