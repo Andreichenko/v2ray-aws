54633ea41d6012460d246a9d0fff32f8b65cf5cb
aleksandrandreichenko <andreichenko@web.de>
2021-06-10 10:30:44 +0300

    backend init file

--- security_group.tf
+++ security_group.tf
@@ -7,9 +7,9 @@ resource "aws_security_group" "v2ray-sg" {
 
   ingress {
     description = "Allow all in"
-    from_port   = 22
-    protocol    = "tcp"
-    to_port     = 22
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
