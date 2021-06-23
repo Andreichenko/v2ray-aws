6402d494e86a48bbd4273e0d990beed4be8300c8
aleksandrandreichenko <andreichenko@web.de>
2021-06-23 13:47:33 +0300

    backend init file

--- providers.tf
+++ providers.tf
@@ -2,5 +2,5 @@ provider "aws" {
   profile = var.profile
   region  = var.region-common
   alias   = "region-common"
-  //need to create modules for each resources
+  //need to create modules for each resources for ec2 and vpc
 }
