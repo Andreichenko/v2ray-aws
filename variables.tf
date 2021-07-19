cc4f017877949bec0b260c343e5d7d566ff1622c
aleksandrandreichenko <andreichenko@web.de>
2021-07-19 10:42:29 +0300

    backend init file

--- variables.tf
+++ variables.tf
@@ -28,9 +28,3 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
 }
 
-
-variable "vpc_enable_dns_support" {
-  description = "Should the DNS resolution be supported?"
-  type        = bool
-  default     = false
-}
