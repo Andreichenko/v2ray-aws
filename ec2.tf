102ab75c60d403bd33c462c72179425a6d2512e2
aleksandrandreichenko <andreichenko@web.de>
2021-07-23 10:27:48 +0300

    backend init file

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ data "aws_ssm_parameter" "linuxAMI-eu-central-1" {
   name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
-#update key-pair for logging into EC2 in eu-central-1
+#update key-pair for logging into EC2 in eu-central-1 region
 resource "aws_key_pair" "common-key" {
   provider   = aws.region-common
   public_key = file("~/.ssh/id_rsa.pub")
