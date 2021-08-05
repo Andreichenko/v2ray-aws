45ce7ffc766aa9df4010c2ed9d3d16d3f34ce1a2
aleksandrandreichenko <andreichenko@web.de>
2021-08-05 10:43:08 +0300

    backend init file

--- network.tf
+++ network.tf
@@ -59,7 +59,7 @@ resource "aws_subnet" "subnet-1c" {
   tags = {
     Name        = "Common subnet primary"
     Owner       = "Aleksandr Andreichenko"
-    Environment = "Production"
+    Environment = "Production Environment"
     Region      = "eu-central-1"
     Zone        = "zone-1c"
   }
