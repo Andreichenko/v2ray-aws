c741be0a9a036c1732d8bf05ce2105f5e44dabaf
aleksandrandreichenko <andreichenko@web.de>
2022-01-18 11:04:02 +0300

    changed type of instance

--- ec2.tf
+++ ec2.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "v2ray-server" {
   root_block_device {
     volume_type           = "gp2"
     volume_size           = 8
-    encrypted             = true
+    encrypted             = false
     delete_on_termination = false
   }
 
