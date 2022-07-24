7743fc45ec82aa4831ae7dd98ce4f6ec41777bd9
Andreichenko <andreichenko@web.de>
2022-07-24 17:50:19 +0300

    fix

--- backend.tf
+++ backend.tf
@@ -8,6 +8,6 @@ terraform {
     region  = "eu-central-1"
     profile = "default"
     key     = "terraform-state-file/statefile.tfstate"
-    bucket  = "terraform-state-cname-bucket-6"
+    bucket  = "terraform-state-cname-bucket-8"
   }
 }
