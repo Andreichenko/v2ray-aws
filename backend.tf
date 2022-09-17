6eed18629a37e3297b44d5d46c043af03b1a7e45
aleksandrandreichenko <andreichenko@web.de>
2022-09-17 10:33:37 +0300

    changed bucket

--- backend.tf
+++ backend.tf
@@ -8,6 +8,6 @@ terraform {
     region  = "eu-central-1"
     profile = "default"
     key     = "terraform-state-file/statefile.tfstate"
-    bucket  = "terraform-state-cname-bucket-8"
+    bucket  = "terraform-state-cname-bucket-6"
   }
 }
