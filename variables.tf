a259056b2e8ae9e616db7f78d64a7641afc3c304
aleksandrandreichenko <andreichenko@web.de>
2023-05-07 23:49:59 +0300

    backend init file

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "instance_type" {
 variable "region-common" {
   type        = string
   default     = "eu-central-1"
-  description = "The common region eu-central-1"
+  description = "The common region eu-central-1 for v2ray"
 }
 
 variable "vpc_name" {
