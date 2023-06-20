cca105b2239eef0d05d1f8b8930d045b6ccd29ac
Andreichenko <andreichenko@web.de>
2023-06-20 10:33:12 +0300

    fix

--- ec2.tf
+++ ec2.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "v2ray-server" {
 
   root_block_device {
     volume_type           = "gp2"
-    volume_size           = 16
+    volume_size           = 20
     encrypted             = false
     delete_on_termination = false
   }
