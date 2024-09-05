a72531091583b685f2bfa164bbf06adade97e046
Andreichenko <andreichenko@web.de>
2024-09-05 05:58:39 -0400

    added some changes

--- ec2.tf
+++ ec2.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "v2ray-server" {
 
   root_block_device {
     volume_type           = "gp2"
-    volume_size           = 500
+    volume_size           = 50
     encrypted             = false
     delete_on_termination = false
   }
