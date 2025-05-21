0902925b7d625bbcf72df8dc6de52a480fcf9279
Andreichenko <andreichenko@web.de>
2025-05-21 10:56:54 +0300

    regular changes for aws

--- ec2.tf
+++ ec2.tf
@@ -23,8 +23,8 @@ resource "aws_instance" "v2ray-server" {
 
   root_block_device {
     volume_type           = "gp3"
-    volume_size           = 500
-    encrypted             = false
+    volume_size           = 50
+    encrypted             = true
     delete_on_termination = false
   }
 
