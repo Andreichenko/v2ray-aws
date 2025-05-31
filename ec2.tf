146efeae80ec9a3f41a5d58b6ba376e4b3a0d1b7
Andreichenko <andreichenko@web.de>
2025-05-31 13:59:19 +0300

    regilar changes for aws

--- ec2.tf
+++ ec2.tf
@@ -23,8 +23,8 @@ resource "aws_instance" "v2ray-server" {
 
   root_block_device {
     volume_type           = "gp3"
-    volume_size           = 10
-    encrypted             = true
+    volume_size           = 100
+    encrypted             = false
     delete_on_termination = false
   }
 
