a5bd7b5fa8f420e08389a49474d0c9f6556f25a6
Andreichenko <andreichenko@web.de>
2025-06-17 16:23:26 +0300

    regilar changes for aws

--- ec2.tf
+++ ec2.tf
@@ -22,9 +22,9 @@ resource "aws_instance" "v2ray-server" {
   subnet_id                   = aws_subnet.subnet-1a.id
 
   root_block_device {
-    volume_type           = "gp3"
-    volume_size           = 200
-    encrypted             = false
+    volume_type           = "gp2"
+    volume_size           = 20
+    encrypted             = true
     delete_on_termination = false
   }
 
