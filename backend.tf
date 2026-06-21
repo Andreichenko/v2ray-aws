926749246b5c57ab4be77bdc377b205f4bc9a300
andreichenko <andreichenko@web.de>
2026-06-21 14:02:47 +0300

    infra: upgrade Terraform required version and aws provider syntax in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,10 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.5.0"
   required_providers {
-    aws = ">=3.0.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 
   backend "s3" {
